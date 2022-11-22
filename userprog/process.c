e3944bd5404515cec705765323e4ec8e4e6e3a02
Ubuntu <ubuntu@ip-172-31-10-107.ap-northeast-2.compute.internal>
2022-11-22 14:25:45 +0000

    thread_create : name을 위한 parsing 추가

--- userprog/process.c
+++ userprog/process.c
@@ -50,6 +50,10 @@ process_create_initd (const char *file_name) {
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 
+	/* thread name parsing*/
+	char *save_ptr, *token;
+	token = strtok_r(file_name, " ", &save_ptr);
+
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
