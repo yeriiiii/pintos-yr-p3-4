02e7610ad24b73ddc6efe911c226c8b6b76cf278
Ubuntu <ubuntu@ip-172-31-9-184.ap-northeast-2.compute.internal>
2022-11-26 12:36:14 +0000

    주석 추가

--- userprog/process.c
+++ userprog/process.c
@@ -60,14 +60,14 @@ process_create_initd (const char *file_name) {
 	if (tid == TID_ERROR)
 		palloc_free_page (fn_copy);
 	// multi-oom 강제 종료된 child list가 있는지 검사하여 있다면 process_wait으로 실패한 프로세스를 회수하자
-	struct list_elem *e;
-	struct thread *t;
-	for (e=list_begin(&thread_current()->childs); e != list_end(&thread_current()->childs); e=list_next(e)){
-		t=list_entry(e, struct thread, child_elem);
-		if (t->exit_status == -1){
-			return process_wait(tid);
-		}
-	}
+	// struct list_elem *e;
+	// struct thread *t;
+	// for (e=list_begin(&thread_current()->childs); e != list_end(&thread_current()->childs); e=list_next(e)){
+	// 	t=list_entry(e, struct thread, child_elem);
+	// 	if (t->exit_status == -1){
+	// 		return process_wait(tid);
+	// 	}
+	// }
 	return tid;
 }
 
@@ -270,10 +270,11 @@ process_wait (tid_t child_tid UNUSED) {
 	if (child_thread == NULL) {
 		return -1;
 	}
-	sema_down(&child_thread->wait_sema); 
-	int exit_status = child_thread->exit_status;
-	list_remove(&child_thread->child_elem); 
-	sema_up(&child_thread->free_sema);  
+	sema_down(&child_thread->wait_sema); // 여기서는 parent가 잠드는 거고
+	int exit_status = child_thread->exit_status;// 여기서부터는 깨어났다.
+    // 깨어나면 child의 exit_status를 얻는다.
+	list_remove(&child_thread->child_elem); // child를 부모 list에서 지운다.
+	sema_up(&child_thread->free_sema);// 내가 받았음을 전달하는 sema  
 	return exit_status;
 }
 
@@ -287,19 +288,19 @@ process_exit (void) {
 	 * TODO: We recommend you to implement process resource cleanup here. */
 
 	// close all open files 
-	// for (int i = 0; i<FDCOUNT_LIMIT; i++){
-	// 	close(i);
-	// }
+	for (int i = 0; i<FDCOUNT_LIMIT; i++){
+		close(i);
+	}
 
 	palloc_free_multiple(curr->fd_table,FDT_PAGES);
 	
-	file_close(curr->running);
+	file_close(curr->running); // load에서 file close -> process_exit할때 close file_deny_write
 
-	sema_up(&curr->wait_sema); 
+	sema_up(&curr->wait_sema); // 종료되었다고 기다리고 있는 부모 thread에게 signal 보냄-> sema_up에서 val을 올려줌
 
-	sema_down(&curr->free_sema); 
+	sema_down(&curr->free_sema); // 부모의 exit_Status가 정확히 전달되었는지 확인(wait)
 	
-	process_cleanup();		
+	process_cleanup();	// pml4를 날림(이 함수를 call 한 thread의 pml4)
 
 }
 
@@ -797,7 +798,7 @@ int process_add_file(struct file *f){
 	struct file **fd_table = cur_thread->fd_table; // 현재 스레드의 파일 디스크립터 테이블
 	int fd = cur_thread->fd; // 현재 스레드의 파일 디스크립터
 
-	while (cur_thread->fd_table[fd] != NULL && fd <FDCOUNT_LIMIT){ // fdt에 빈 자리가 날 때까지 fd 값을 계속 1씩 올린다. 그래서 자리가 나면 해당 자리에 파일을 배치하고 해당 디스크립터 값(=fdt의 인덱스)를 반환한다.
+	while (cur_thread->fd_table[fd] && fd <FDCOUNT_LIMIT){ // fdt에 빈 자리가 날 때까지 fd 값을 계속 1씩 올린다. 그래서 자리가 나면 해당 자리에 파일을 배치하고 해당 디스크립터 값(=fdt의 인덱스)를 반환한다.
 		fd++;
 	}
 
